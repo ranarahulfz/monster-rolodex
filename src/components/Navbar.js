88942fd98715835a004adbf22889dc789971db35
ranarahulfz <ranarahulfz@gmail.com>
2019-08-07 18:50:30 +0530

    Adding navbar to the app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import styled from 'styled-components'
+
+const Link = styled.a`
+    font-family: Roboto;
+    font-weight: 600;
+` 
 
 function Navbar () {
     return(
@@ -7,18 +13,18 @@ function Navbar () {
             <Layout fixedHeader>
                 <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
                     <Navigation>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
                     </Navigation>
                 </Header>
                 <Drawer title="Title">
                     <Navigation>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
+                        <Link href="#">Link</Link>
                     </Navigation>
                 </Drawer>
                 <Content />
