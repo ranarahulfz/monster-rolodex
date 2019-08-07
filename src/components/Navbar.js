0521a134b890c6dd56ba50044bbdf0eb994756d1
ranarahulfz <ranarahulfz@gmail.com>
2019-08-07 19:29:13 +0530

    Adding navbar to the app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,39 @@ import styled from 'styled-components'
 const Link = styled.a`
     font-family: Roboto;
     font-weight: 600;
+    text-transform: uppercase;
+    color: rgba(0,0,0,0.87);
+    font-family: "Roboto Mono",monospace;
 ` 
 
 function Navbar () {
     return(
         <div style={{height: '300px', position: 'relative'}}>
-            <Layout fixedHeader>
-                <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            <Layout>
+                <Header 
+                    style={{
+                        backgroundColor: '#fff',
+                        borderBottom: '1px solid #E0E0E0'
+                    }}
+                    title = {
+                        <span
+                            style = {{
+                                fontSize: '20px',
+                                letterSpacing: '.4px',
+                                paddingLeft: '24px',
+                                textTransform: 'uppercase',
+                                color: 'rgba(0,0,0,0.87)',
+                                fontFamily: '"Roboto Mono",monospace' 
+                            }}
+                        >
+                            <span>FAST </span>
+                            <strong>BSR</strong>
+                        </span>}>
                     <Navigation>
-                        <Link href="#">Link</Link>
-                        <Link href="#">Link</Link>
-                        <Link href="#">Link</Link>
-                        <Link href="#">Link</Link>
+                        <Link style={{color: 'rgba(0,0,0,0.87)'}} href="#">Link</Link>
+                        <Link style={{color: 'rgba(0,0,0,0.87)'}} href="#">Link</Link>
+                        <Link style={{color: 'rgba(0,0,0,0.87)'}} href="#">Link</Link>
+                        <Link style={{color: 'rgba(0,0,0,0.87)'}} href="#">Link</Link>
                     </Navigation>
                 </Header>
                 <Drawer title="Title">
